da7dc598c2648ddd3dfac24c233e03f75244feaa
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-02-18 19:26:57 -0300

    Add exercicio medias alunos

---
+++ src/operadores/usandofor.java
@@ -0,0 +1,7 @@
+public class usandofor {
+    public static void main(String[] args) {
+        for (int primeiroNumero = 1; primeiroNumero <= 10; primeiroNumero++) {
+            System.out.println("Numero: " + primeiroNumero);
+        }
+    }
+}
