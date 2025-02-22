f92d193a0d2aa4d483d57590ceb22c9d18cb3c1c
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-02-22 10:21:48 -0300

    par ou impar utilizando if or else

---
+++ src/operadores/Paresvinte.java
@@ -0,0 +1,12 @@
+public class Paresvinte {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 50) {
+            if (numero % 2 == 0) {
+                System.out.println("Número par: " + numero);
+            }
+            numero++;
+        }
+    }
+}
