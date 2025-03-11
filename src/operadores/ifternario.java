6c5d5928c75a3cbec3ba979ec9fdd6082667857c
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-03-11 10:01:50 -0300

    if ternario

---
+++ src/operadores/ifternario.java
@@ -0,0 +1,11 @@
+package operadores;
+
+public class ifternario {
+    public static void main(String[] args) {
+        int age = 10;
+
+        String texto = age >= 18 ? "maior de idade" : "menor de idade";
+
+        System.out.println(texto);
+    }
+}
